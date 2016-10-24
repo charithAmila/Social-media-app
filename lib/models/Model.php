c256b109058ba0bf4344d59f32e952118643d0df
Charith Dissanayaka <charith@ascentura.com>
2016-10-24 10:40:43 +0530

    Parent Model

--- lib/models/Model.php
+++ lib/models/Model.php
@@ -1,10 +1,17 @@
 <?php
 namespace models;
-
+/**
+ * Model is parent for FacebookPosts and Twitter Posts
+ */
 class Model
 {
     public $media;
-        
+    /*
+     * Validate youser post is empty or not
+     * @return type true or false
+     * if validate is pass return true
+     * if not validate is pass return false
+     */
     public function validator()
     {
         if(!strlen(trim($this->text)))
