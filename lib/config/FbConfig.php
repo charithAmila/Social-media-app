d3b6d8c1ec8104c371246d95f51f855c994aae4e
Charith Dissanayaka <charith@ascentura.com>
2016-10-24 10:29:23 +0530

    fb config

--- lib/config/FbConfig.php
+++ lib/config/FbConfig.php
@@ -1,15 +1,21 @@
 <?php
 namespace config;
-
+/**
+ * FbConfig class provide facebook user configuration data.
+ */
 class FbConfig
 {
+    /**
+     * Privide User Facebook Credentials
+     * @return type array
+     */
     public static function configData()
-        {
-            return [
-                    'appId'=>'1817835078503800',
-                    'secret'=>'49045433cc22c5b547f213f568366007',
-                    'accessToken'=>'EAAZA1T7YdTXgBAD7oQ1QuVV8j3J7sdjS7PudfDrZAAZC7mEyuF6pWiFcfzyzU4qg6dktvBxcdVgNJmsA2PxHe9DW1YnV2T82sAxhtwP8MnZBCL7BxMm9CbZByHkMuXCN09TZCwIOmtzAjg7svtoOxfCOkozs8FOAxI5wWgCbXxZCAZDZD',
-		];
-        }
+    {
+        return [
+                'appId'=>'Your app id', // appId get from your facebook app
+                'secret'=>'Your secrt', // secret get from your facebook app
+                'accessToken'=>'your access Token', // accessToken get from your facebook app
+               ];
+   }
 }
 
