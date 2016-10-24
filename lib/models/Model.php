51d315138b63cf660fe87639cdf4f5b158df404e
Charith Dissanayaka <charith@ascentura.com>
2016-10-24 10:06:37 +0530

    Model file

--- lib/models/Model.php
+++ lib/models/Model.php
@@ -4,17 +4,14 @@ namespace models;
 class Model
 {
     public $media;
-    public function __construct() 
-    {
         
-    }
-    
     public function validator()
     {
         if(!strlen(trim($this->text)))
         {
             return false;
-        }  else 
+        }  
+        else 
         {
             return true;
         }
