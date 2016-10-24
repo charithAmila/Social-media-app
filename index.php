fdde25980e92ca2a092bbff559ab0c400d18596c
Charith Dissanayaka <charith@ascentura.com>
2016-10-24 11:00:35 +0530

    index

--- index.php
+++ index.php
@@ -40,15 +40,11 @@
                       <button class="btn btn-sm btn-info">POST</button>
                   </form>
 	      </div>
-      </div><!-- /.row -->
-    </div><!-- /.container -->
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
+      </div>
+    </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
     <script src="public/js/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="public/js/ie10-viewport-bug-workaround.js"></script>
   </body>
 </html>
