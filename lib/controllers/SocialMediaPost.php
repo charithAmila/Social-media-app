e49ec50674e09972a82f3a53a788e1c74c31740f
Charith Dissanayaka <charith@ascentura.com>
2016-10-24 10:18:13 +0530

    controller

--- lib/controllers/SocialMediaPost.php
+++ lib/controllers/SocialMediaPost.php
@@ -4,30 +4,34 @@ use models\TwitterPosts;
 use models\FaceBookPosts;
 
 /**
- * SocialMediaPost controller implements the authenticate and post to social media for TwitterPosts model.
+ * SocialMediaPost controller implements the  post to social media for TwitterPosts,FaceBookPosts models.
  */
 class SocialMediaPost
 {
     
-
+    /**
+     * Create a new twitter post
+     * if post is success return success message
+     * if post is fail return error message
+     * @param type $msg
+     * @return type string
+     */
     public function TwitterPost($msg)
     {
         $model = new TwitterPosts();
         $model->text = $msg;
         if($model->validator())
         {
-            return $model->StatusUpdate();
-//            if($model->StatusUpdate()){
-//                header('Location: ../../?tw=success');
-//            }
-//            else
-//            {
-//                header('Location: ../../?tw=error');
-//            }
-                
+            return $model->StatusUpdate();                
         }
     }
-    
+    /**
+     * Create a new facebook post
+     * if post is success return success message
+     * if post is fail return error message
+     * @param type $msg
+     * @return type string
+     */
     public function FbPost($msg)
     {
         $model = new FaceBookPosts();
