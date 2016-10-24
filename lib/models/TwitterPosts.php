534c717a84452b5dd035ff9def21a2b1f61bed69
Charith Dissanayaka <charith@ascentura.com>
2016-10-24 10:54:57 +0530

    TwitterPostModel

--- lib/models/TwitterPosts.php
+++ lib/models/TwitterPosts.php
@@ -5,11 +5,13 @@ use models\Model;
 use TwitterAPIExchange;
 use config\TwitterConfig;
 
-
+/**
+ * TwitterPosts is the model behind the twitter posts.
+ */
 class TwitterPosts extends Model
 {
     public $text;
-    public $conf_array = [];
+    private $conf_array = [];
 
 
     public function __construct()
@@ -17,34 +19,31 @@ class TwitterPosts extends Model
         $this->conf_array = TwitterConfig::configData();
     }
 
-
+    /**
+     * Send posts to the twitter connection with twitter api
+     * @return string
+     */
     public function StatusUpdate()
     {
-        $data = [];
         $settings = array('oauth_access_token' => $this->conf_array['oauth_access_token'],
 		'oauth_access_token_secret' => $this->conf_array['oauth_access_token_secret'],
 		'consumer_key' => $this->conf_array['consumer_key'],
 		'consumer_secret' => $this->conf_array['consumer_secret']
 		);
 		
-		$url = $this->conf_array['url_postStatusUpdate'];
-		try
-		{
-			$twitter = new TwitterAPIExchange($settings);
-		
-			$requestMethod 	= 'POST';
-			$postfields 	= array('status' => $this->text); 
-		
-			$twitter->buildOauth($url, $requestMethod)
-			->setPostfields($postfields)
-			->performRequest();
-                        return 'sucess';
-		}
-		catch(Exception $e)
-		{
-                        return 'error';
-		}
-		
+	$url = $this->conf_array['url_postStatusUpdate'];
+	try
+	{
+	$twitter = new TwitterAPIExchange($settings);
+	$requestMethod 	= 'POST';
+	$postfields = array('status' => $this->text); 
+	$twitter->buildOauth($url, $requestMethod)->setPostfields($postfields)->performRequest();
+           return 'sucess';
+	}
+	catch(Exception $e)
+	{
+            return 'error';
+	}
 		
 	}
     }
